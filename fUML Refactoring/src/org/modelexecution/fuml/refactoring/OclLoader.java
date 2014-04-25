31500bece3d3a108e52f5f3b367f27fb8f6fb110
Sebastian Geiger <sbastig@gmx.net>
2014-04-25 16:09:33 +0200

    Updates

--- fUML Refactoring/src/org/modelexecution/fuml/refactoring/OclLoader.java
+++ fUML Refactoring/src/org/modelexecution/fuml/refactoring/OclLoader.java
@@ -1,10 +1,7 @@
 package org.modelexecution.fuml.refactoring;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.InputStream;
 import java.util.List;
 
@@ -17,52 +14,22 @@ import org.eclipse.emf.ecore.EOperation;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EParameter;
 import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.ocl.OCLInput;
 import org.eclipse.ocl.ParserException;
 import org.eclipse.ocl.ecore.CallOperationAction;
+import org.eclipse.ocl.ecore.Constraint;
 import org.eclipse.ocl.ecore.EcoreEnvironmentFactory;
 import org.eclipse.ocl.ecore.SendSignalAction;
+import org.eclipse.ocl.expressions.OCLExpression;
+import org.eclipse.ocl.helper.OCLHelper;
 import org.eclipse.ocl.uml.OCL;
-import org.eclipse.ocl.uml.UMLEnvironmentFactory;
-import org.eclipse.uml2.uml.Constraint;
 import org.eclipse.uml2.uml.UMLPackage;
 import org.eclipse.uml2.uml.resource.UMLResource;
 
 public class OclLoader {
 
-	public List<Constraint> loadOcl() {
-		URI uri = URI.createFileURI("models/extractSuperclass/extractSuperclass.uml");
-		
-		final ResourceSet set = new ResourceSetImpl();
-		/* We need to register a resource factory to avoid a runtime exception */
-		set.getPackageRegistry().put(UMLPackage.eNS_URI, UMLPackage.eINSTANCE);
-		set.getResourceFactoryRegistry().getExtensionToFactoryMap()
-			.put(UMLResource.FILE_EXTENSION, UMLResource.Factory.INSTANCE);
-		
-		/* we use the environment factory to load the uml model */
-		UMLEnvironmentFactory umlEnvFactory = new UMLEnvironmentFactory();
-		umlEnvFactory.loadEnvironment(set.getResource(uri,true));
-		
-		/* we pass the factory instance to OCL */
-		OCL ocl = OCL.newInstance(umlEnvFactory);
-		
-		//Load file stream for ocl file
-		InputStream stream;
-		try {
-			stream = new FileInputStream("models/extractSuperclass/extractSuperclass_uml.ocl");
-			OCLInput document = new OCLInput(stream);
-			List<Constraint> constraints = ocl.parse(document);
-			return constraints; }
-		catch (FileNotFoundException e) { e.printStackTrace(); }
-		catch (ParserException e) { e.printStackTrace(); }
-		return null;
-	}
-	
 	public List<org.eclipse.ocl.ecore.Constraint> loadEcoreOcl() {
 		URI uri = URI.createFileURI("models/extractSuperclass/extractSuperclass.uml");
 		final ResourceSet set = new ResourceSetImpl();
@@ -78,6 +45,25 @@ public class OclLoader {
 		                    EObject,CallOperationAction,SendSignalAction,org.eclipse.ocl.ecore.Constraint,EClass,EObject>
 			ocl = OCL.newInstance(EcoreEnvironmentFactory.INSTANCE);
 		
+		boolean valid = false;
+		OCLExpression<EClassifier> query = null;
+		try {
+		    // create an OCL helper object
+		    OCLHelper<EClassifier, ?, ?, Constraint> helper = ocl.createOCLHelper();
+		    // set the OCL context classifier
+		    helper.setContext(null);
+		    query = helper.createQuery("true");
+		    
+		    valid = true;
+		} catch ( ParserException e) {
+			e.printStackTrace();
+		}
+
+		if(valid) {
+			System.out.println("true");
+		}
+		
+		
 		//Load file stream for ocl file
 		InputStream stream;
 		try {
@@ -89,13 +75,13 @@ public class OclLoader {
 		catch (ParserException e) { e.printStackTrace(); }
 		return null;
 	}
-	
+
 	public static void main(String[] args) {
 		OclLoader loader = new OclLoader();
 		// Variante 1: OCL mit UMLEnvironmentFactory:
-		loader.loadOcl();
+		// loader.loadOcl();
 		// Variante 2: OCL mit EcoreEnvironmentFactory:
-		//loader.loadEcoreOcl();
+		loader.loadEcoreOcl();
 	}
-	
+
 }
