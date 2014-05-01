baf8c825e107519b851274abd0dbf6639d923249
Kristof Meixner <kristof.meixner@fatlenny.net>
2014-05-01 14:26:23 +0200

    Added postconstraint check (fails atm)

--- fUML Refactoring/src/org/modelexecution/fuml/refactoring/experiments/SimpleModelModificationTest.java
+++ fUML Refactoring/src/org/modelexecution/fuml/refactoring/experiments/SimpleModelModificationTest.java
@@ -1,5 +1,8 @@
 package org.modelexecution.fuml.refactoring.experiments;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
@@ -30,16 +33,18 @@ import org.junit.Test;
 public class SimpleModelModificationTest {
     private static final String MODEL_PATH = "models/extractSuperclass/extractSuperclass.uml";
 
-    private static final String OCL_CONSTRAINT = "name.at(1)=name.at(1).toUpperCase()";
+    private static final String OCL_PRE_CONSTRAINT = "self.name <> '%s'";
+    // FIXME Query works in OCL console but returns an exception here.
+    private static final String OCL_POST_CONSTRAINT = "self.oclIsKindOf(%s)";
 
     /** The current resource. */
     private ResourceSet resourceSet;
     private Resource resource;
+    private OCL<?, EClassifier, ?, ?, ?, ?, ?, ?, ?, Constraint, EClass, EObject> ocl;
+    private OCLHelper<EClassifier, ?, ?, Constraint> helper;
 
     /**
-     * Load a UML Model file and initialize a resource set.
-     * 
-     * @param umlModelFile
+     * Initialize a {@link ResourceSet}, load an UML model file and retrieve a {@link Resource}.
      */
     @Before
     public void initializeResourceSet() {
@@ -52,6 +57,20 @@ public class SimpleModelModificationTest {
         resource = resourceSet.getResource(uri, true);
     }
 
+    /**
+     * Initialize {@link OCL} and {@link OCLHelper} for queries.
+     */
+    @Before
+    public void initializeOCLInfrastructure() {
+        ocl = OCL.newInstance(EcoreEnvironmentFactory.INSTANCE);
+        helper = ocl.createOCLHelper();
+    }
+
+    /**
+     * Loads the root {@link Model} of an UML file.
+     * 
+     * @return the root {@link Model}.
+     */
     private Model loadModel() {
         TreeIterator<EObject> iterator = resource.getAllContents();
         while (iterator.hasNext()) {
@@ -63,54 +82,11 @@ public class SimpleModelModificationTest {
         return null;
     }
 
-    @Test
-    public void test() {
-        Model model = loadModel();
-        String superClassName = "Vehicle";
-        Class superClass = model.createOwnedClass(superClassName, false);
-
-        System.out.println(checkPreconstraints(model, superClassName));
-
-        Set<Class> contents = loadAllClasses();
-
-        for (Class clazz : contents) {
-            String className = clazz.getName();
-            if (className.equals("Truck") || className.equals("Car")) {
-                clazz.createGeneralization(superClass);
-            }
-        }
-
-        TreeIterator<EObject> iterator = resource.getAllContents();
-        while (iterator.hasNext()) {
-            EObject eObject = iterator.next();
-            System.out.println(eObject.toString());
-        }
-
-    }
-
-    private boolean checkPreconstraints(Model model, String name) {
-        OCL<?, EClassifier, ?, ?, ?, ?, ?, ?, ?, Constraint, EClass, EObject> ocl;
-        ocl = OCL.newInstance(EcoreEnvironmentFactory.INSTANCE);
-        OCLHelper<EClassifier, ?, ?, Constraint> helper = ocl.createOCLHelper();
-        helper.setContext(UMLPackage.eINSTANCE.getClass_());
-        OCLExpression<EClassifier> query = null;
-
-        try {
-            query = helper.createQuery("self.name <> '" + name + "'");
-            Query<EClassifier, EClass, EObject> eval = ocl.createQuery(query);
-
-            for (Class clazz : loadAllClasses()) {
-                if (!eval.check(clazz)) {
-                    return false;
-                }
-            }
-        } catch (ParserException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return true;
-    }
-
+    /**
+     * Loads all {@link Class} instances except of {@link Activity} instances from the loaded {@link Resource}.
+     * 
+     * @return a {@link Set} that contains all {@link Class} instances.
+     */
     private Set<Class> loadAllClasses() {
         Set<Class> contents = new HashSet<>();
         TreeIterator<EObject> iterator = resource.getAllContents();
@@ -121,6 +97,83 @@ public class SimpleModelModificationTest {
             }
         }
         return contents;
+    }
+
+    @Test
+    public void testSuperclassExtrationWithPreAndPostChecks_shouldSucceed() {
+        Model model = loadModel();
+        String superClassName = "Vehicle";
+
+        try {
+            assertTrue(checkPreconstraints(superClassName));
+        } catch (ParserException pre) {
+            fail("Preconstraints failed with ParserException");
+        }
+
+        Class superClass = model.createOwnedClass(superClassName, false);
+        Set<Class> contents = loadAllClasses();
+
+        for (Class clazz : contents) {
+            String className = clazz.getName();
+            if (className.equals("Truck") || className.equals("Car")) {
+                clazz.createGeneralization(superClass);
+            }
+        }
+
+        try {
+            assertTrue(checkPostconstraints(superClass));
+        } catch (ParserException post) {
+            post.printStackTrace();
+            fail("Postconstraints failed with ParserException");
+        }
+
+        TreeIterator<EObject> iterator = resource.getAllContents();
+        while (iterator.hasNext()) {
+            EObject eObject = iterator.next();
+            System.out.println(eObject.toString());
+        }
+
+    }
+
+    /**
+     * Checks the preconstraints for the superclass extraction.
+     * 
+     * @param name of the new class to be created.
+     * @return {@code true} if the preconstraints are met.
+     * @throws ParserException an exception if the preconditions are incorrectly formulated.
+     */
+    private boolean checkPreconstraints(String name) throws ParserException {
+        helper.setContext(UMLPackage.eINSTANCE.getClass_());
+
+        OCLExpression<EClassifier> query = helper.createQuery(String.format(OCL_PRE_CONSTRAINT, name));
+        Query<EClassifier, EClass, EObject> eval = ocl.createQuery(query);
+
+        for (Class clazz : loadAllClasses()) {
+            if (!eval.check(clazz)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks the postconstraints for the superclass extraction.
+     * 
+     * @param name of the new class to be created.
+     * @return {@code true} if the preconstraints are met.
+     * @throws ParserException an exception if the preconditions are incorrectly formulated.
+     */
+    private boolean checkPostconstraints(Class superClass) throws ParserException {
+        helper.setContext(UMLPackage.eINSTANCE.getClass_());
+
+        for (Class clazz : loadAllClasses()) {
+            if (clazz.getName().equals("Truck") || clazz.getName().equals("Car")) {
+                // FIXME This query can not be parsed due to a SemanticException with message
+                // "Unknown enumeration literal (Vehicle)"
+                helper.createQuery(String.format(OCL_POST_CONSTRAINT, superClass.getQualifiedName()));
+            }
+        }
+        return true;
     }
 
 }
