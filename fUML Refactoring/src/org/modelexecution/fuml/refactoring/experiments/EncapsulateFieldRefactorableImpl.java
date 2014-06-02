2d38fe0566dd6e28ef3f8afa7a721d8a8bc2ab11
Kristof Meixner <kristof.meixner@fatlenny.net>
2014-06-02 11:36:25 +0200

    Extended encapsulate field refactoring to create activity

--- fUML Refactoring/src/org/modelexecution/fuml/refactoring/experiments/EncapsulateFieldRefactorableImpl.java
+++ fUML Refactoring/src/org/modelexecution/fuml/refactoring/experiments/EncapsulateFieldRefactorableImpl.java
@@ -1,16 +1,9 @@
 package org.modelexecution.fuml.refactoring.experiments;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EParameter;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.ocl.OCL;
 import org.eclipse.ocl.ParserException;
 import org.eclipse.ocl.Query;
@@ -29,8 +22,6 @@ import org.eclipse.uml2.uml.ControlFlow;
 import org.eclipse.uml2.uml.InputPin;
 import org.eclipse.uml2.uml.LiteralBoolean;
 import org.eclipse.uml2.uml.LiteralInteger;
-import org.eclipse.uml2.uml.Model;
-import org.eclipse.uml2.uml.NamedElement;
 import org.eclipse.uml2.uml.ObjectFlow;
 import org.eclipse.uml2.uml.Operation;
 import org.eclipse.uml2.uml.OutputPin;
@@ -68,43 +59,6 @@ public class EncapsulateFieldRefactorableImpl implements Refactorable {
     }
 
     /**
-     * Loads the root {@link Model} of an UML file.
-     * 
-     * @return the root {@link Model}.
-     */
-    private Model loadModel(Resource resource) {
-        TreeIterator<EObject> iterator = resource.getAllContents();
-        while (iterator.hasNext()) {
-            EObject eObject = iterator.next();
-            if (eObject instanceof Model) {
-                return (Model) eObject;
-            }
-        }
-        return null;
-    }
-
-    /**
-     * Loads all {@link Class} instances except of {@link Activity} instances from the loaded {@link Resource}.
-     * 
-     * @return a {@link Set} that contains all {@link Class} instances.
-     */
-    private Set<Class> loadAllClasses(Class selectedElement) {
-
-        EList<NamedElement> elements = selectedElement.getPackage().getMembers();
-        selectedElement.getOwner();
-
-        Set<Class> contents = new HashSet<>();
-        Iterator<NamedElement> iterator = elements.iterator();
-        while (iterator.hasNext()) {
-            NamedElement element = iterator.next();
-            if (element instanceof Class && !(element instanceof Activity)) {
-                contents.add((Class) element);
-            }
-        }
-        return contents;
-    }
-
-    /**
      * Checks the preconstraints for the superclass extraction.
      * 
      * @param name of the new class to be created.
@@ -131,9 +85,8 @@ public class EncapsulateFieldRefactorableImpl implements Refactorable {
 
         query = helper.createQuery(OCL_PRE_CONSTRAINT);
 
-        Property property = (Property) data.get("selectedElement");
-        Type propertyType = property.getType();
-        String propertyName = property.getName();
+        Type propertyType = selectedElement.getType();
+        String propertyName = selectedElement.getName();
         String normalizedName = propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1);
 
         setOperation = UMLFactory.eINSTANCE.createOperation();
@@ -159,7 +112,6 @@ public class EncapsulateFieldRefactorableImpl implements Refactorable {
      */
     @Override
     public boolean performRefactoring() throws RefactoringException {
-        // Model model = loadModel(resource);
         Property selectedElement = (Property) data.get("selectedElement");
 
         Class class_ = selectedElement.getClass_();
@@ -183,6 +135,12 @@ public class EncapsulateFieldRefactorableImpl implements Refactorable {
         // self.member->select(c|c.oclIsTypeOf(Class)).oclAsType(Class).member->
         // select(a|a.oclIsTypeOf(Activity)).oclAsType(Activity).node->
         // select(n|n.oclIsTypeOf(ReadStructuralFeatureAction)).oclAsType(ReadStructuralFeatureAction).structuralFeature
+
+        // Get all AddStructuralFeatureValueActions
+        // Context Package
+        // self.member->select(c|c.oclIsTypeOf(Class)).oclAsType(Class).member->
+        // select(a|a.oclIsTypeOf(Activity)).oclAsType(Activity).node->
+        // select(n|n.oclIsTypeOf(AddStructuralFeatureValueAction)).oclAsType(AddStructuralFeatureValueAction).structuralFeature
         return true;
     }
 
@@ -321,24 +279,24 @@ public class EncapsulateFieldRefactorableImpl implements Refactorable {
     public boolean checkPostCondition() throws ParserException {
         helper.setContext(UMLPackage.eINSTANCE.getClass_());
 
-        Class selectedElement = (Class) data.get("selectedElement");
-        Class superClass = (Class) data.get("newSuperClass");
+        Property selectedElement = (Property) data.get("selectedElement");
 
-        if (superClass == null) {
-            return false;
-        }
+        Variable<EClassifier, EParameter> getVariable = ExpressionsFactory.eINSTANCE.createVariable();
+        getVariable.setName("getOperation");
+        getVariable.setType(UMLPackage.Literals.PROPERTY);
+        ocl.getEnvironment().addElement(getVariable.getName(), getVariable, true);
 
-        Variable<EClassifier, EParameter> variable = ExpressionsFactory.eINSTANCE.createVariable();
-        variable.setName("newSuperClass");
-        variable.setType(UMLPackage.Literals.CLASSIFIER);
-        ocl.getEnvironment().addElement(variable.getName(), variable, true);
+        Variable<EClassifier, EParameter> setVariable = ExpressionsFactory.eINSTANCE.createVariable();
+        setVariable.setName("setOperation");
+        setVariable.setType(UMLPackage.Literals.PROPERTY);
+        ocl.getEnvironment().addElement(setVariable.getName(), setVariable, true);
+
         OCLExpression<EClassifier> query = helper.createQuery(OCL_POST_CONSTRAINT);
         Query<EClassifier, EClass, EObject> eval = ocl.createQuery(query);
-        eval.getEvaluationEnvironment().add("newSuperClass", superClass);
-        if (!eval.check(selectedElement)) {
-            return false;
-        }
 
-        return true;
+        eval.getEvaluationEnvironment().add("getOperation", getOperation);
+        eval.getEvaluationEnvironment().add("setOperation", setOperation);
+
+        return eval.check(selectedElement);
     }
 }
